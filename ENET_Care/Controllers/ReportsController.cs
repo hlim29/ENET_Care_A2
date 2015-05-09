c930797dece56bd9434cc379aaa1fa2eaa3df164
amirelemam <amirelemam@email.com>
2015-05-09 21:19:04 +1000

    Views created in HTML

---
+++ ENET_Care/Controllers/ReportsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ENET_Care.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DistributionCentreStockReport()
+        {
+            return View();
+        }
+
+        public ActionResult DistributionCentreLossesReport()
+        {
+            return View();
+        }
+
+        public ActionResult DoctorActivityReport()
+        {
+            return View();
+        }
+
+        public ActionResult ValueInTransit()
+        {
+            return View();
+        }
+
+        public ActionResult GlobalStock()
+        {
+            return View();
+        }
+
+        public ActionResult MyInformation()
+        {
+            return View("Pages", MyInformation());
+        }
+    }
+}
