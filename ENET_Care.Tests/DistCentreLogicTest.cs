5017eade4fa8a1b2f5a6c0eaccd9ac4cd93afb78
AlbertSamuel <albertsamuelhussen@gmail.com>
2015-05-14 21:59:50 +1000

    Adding DistCentreLogicTest

---
+++ ENET_Care.Tests/DistCentreLogicTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENET_Care.BusinessLogic;
+using ENET_Care.Data;
+using System.Diagnostics;
+
+namespace ENET_Care.Tests
+{
+    [TestClass]
+    public class DistCentreLogicTest
+    {
+        /// <summary>
+        /// Currently its hardcoded to 3
+        /// </summary>
+        [TestMethod]
+        public void DistCentreLogicTest_GetAllDistCentre_CountEqualsThree()
+        {
+            int totalDistCentre = DistCentreLogic.GetAllDistCentre().Count;
+            Assert.Equals(3,totalDistCentre);
+        }
+    }
+}
