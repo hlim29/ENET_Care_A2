23ad18c81b2b93ed7f8de9dd5abac0117867f518
Henry Lim <henry.lim29@gmail.com>
2015-05-21 23:51:11 +1000

    Cleaned up dependencies

--- ENET_Care/App_Start/BundleConfig.cs
+++ ENET_Care/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ENET_Care
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-1.10.2.js"));
+                        "~/Scripts/jquery-2.1.4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
